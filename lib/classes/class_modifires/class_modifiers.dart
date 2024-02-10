2ad4cc33dc528f4fd72de6a7dd039b39cce6f9c6
maaz-afridi20 <maazafridi.ma47@gmail.com>
2024-02-10 11:16:32 +0500

    sealed,final,abstract,base,and interface class

--- lib/classes/class_modifires/class_modifiers.dart
+++ lib/classes/class_modifires/class_modifiers.dart
@@ -1,4 +1,30 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  Animal animal = Cat();
+
+  switch (animal) {
+    case Dog():
+      print('dog');
+    case Cat():
+      print('Cat');
+    case Human():
+      print('Human');
+  }
+}
+
+sealed class Animal {}
+
+class Human implements Animal {}
+
+class Dog implements Animal {}
+
+class Cat extends Animal {}
+
+
+
+
+
+
+
 
 
 
@@ -22,9 +48,17 @@ we use it before when we creating a abstract class.
 !Some of the class modifiers are given below.
 
 !sealed class
+sealed class is also just like the abstract class
+because we also cannot instantiate the sealed class just like the abstract class
+when we create a sealed class and when we call the sealed class
+its constructor cannot be called just like other classes
+
 !final class
 !base class
+base class cannot be implemented but it can be extended.
 !interface class
+the interface class is like reverse of the base calss it cannot be
+extended but it can be implemented.
 !mixin class
 
 */  
